d06942d236f1e389f7701aec5be05b8dc9bc9763
vaibhavjain2024 <vaibhav.jain05@nagarro.com>
2025-01-16 13:58:47 +0530

    add qpulse session_helper

--- iot-vendor/cloud/iot_vendor/VENDOR/session/Qpulse/session_helper.py
+++ iot-vendor/cloud/iot_vendor/VENDOR/session/Qpulse/session_helper.py
@@ -1,7 +1,8 @@
+from QP.repositories.db_setup import get_session, create_tables_and_indexes_for_schema
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from PG.repositories.db_setup import get_session
-from VENDOR.helper.session_interface import SecretFacade
+from QP.helper.session_interface import SecretFacade
+import os
 
 # SessionHelper storage
 session_helper = {}
@@ -24,6 +25,9 @@ def get_session_helper(vendor_name, secret_arn, region, timeout=None):
 class SessionHelper:
     def __init__(self, db_connection_string, timeout=None):
         """Initialize the SQLAlchemy engine with the connection string."""
+        statement_timeout = int(os.environ.get("statement_timeout", 900000))
+        work_mem = os.environ.get("work_mem", "4MB")
+
         connect_args = {}
         if timeout:
             connect_args["options"] = f"-c statement_timeout={timeout}"
@@ -31,18 +35,32 @@ class SessionHelper:
         self.engine = create_engine(
             db_connection_string,
             pool_size=30,
-            connect_args=connect_args  # Timeout in milliseconds
+            connect_args={
+                "options": f"-c statement_timeout={statement_timeout} -c work_mem={work_mem}"
+            }
         )
 
-    def get_session(self):
-        """Create and return a new database session."""
+    def get_session(self, shop_id=None, create_tables_and_indexes=False):
         Session = sessionmaker(bind=self.engine)
-        session = Session()
-        self.session = get_session(session, self.engine)
-        return self.session
+        schema_name = None
+        if shop_id:
+            schema_name = f'SHOP_{shop_id}'
+            schema_engine = self.engine.execution_options(
+                schema_translate_map={None: schema_name}
+            )
+            session = Session(bind=schema_engine)
+            session.connection(
+                execution_options={"schema_translate_map": {None: schema_name}}
+            )
+            if create_tables_and_indexes:
+                create_tables_and_indexes_for_schema(self.engine, schema_name)
+        else:
+            session = Session()
+            session = get_session(session, self.engine)
+        return session
 
 
-def get_sessions(vendor_name, secret_arn, region="ap-south-1", timeout=None):
+def get_sessions(vendor_name, secret_arn, shop_id=None, region="ap-south-1", timeout=None):
     """Retrieve a session for the given vendor.
 
     Args:
@@ -55,6 +73,6 @@ def get_sessions(vendor_name, secret_arn, region="ap-south-1", timeout=None):
     """
 
     vendor_session_helper = get_session_helper(vendor_name, secret_arn, region, timeout)
-    vendor_session = vendor_session_helper.get_session()
+    vendor_session = vendor_session_helper.get_session(shop_id = shop_id)
 
     return vendor_session
